60cef61299d86eb814ce9ee9edc3170913ed4c25
Droidna405 <joemwangala@gmail.com>
2022-09-08 14:35:27 -0700

    101 intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints to the standard error
+ * main - print to the standard error
  *
  * Return: one
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
