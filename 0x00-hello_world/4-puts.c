99cc1d8608cf7cb5426731aaa67231ad532bb0dc
Droidna405 <joemwangala@gmail.com>
2022-09-08 14:02:30 -0700

    my first string with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/**  * main - Print to screen 
-* Return: zero  */
+/**
+ * main - print to screen
+ *
+ * Return: zero
+ */
 int main(void) 
 {
 char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts (Mystr);
+ puts(Mystr);
 return (0);
 }
