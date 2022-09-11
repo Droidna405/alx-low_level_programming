dd5ffef0f5f749fe16903de91b55cacd4521f45e
Droidna405 <joemwangala@gmail.com>
2022-09-11 12:59:10 -0700

    loweercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,10 @@
  *
  * Return: zero
  */
-int main (void)
+int main(void)
 {
 char ch = 'a';
-for (ch= 'a'; ch<= 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
