619ba40ab44bfc3728f37be46dce26f8bf7be50c
Droidna405 <joemwangala@gmail.com>
2022-09-12 14:23:05 +0000

    Uppercase and lowercase programme

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-  char ch;
-  for (ch = 'a' ; ch <= 'z' ; ch++)
-    {
-      putchar(ch);
-    }
-  for (ch = 'A' ; ch <= 'Z' ; ch++)
-    {
-      putchar (ch);
-    }
-  putchar('\n');
-  return (0);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar (ch);
+}
+putchar('\n');
+return (0);
 }
