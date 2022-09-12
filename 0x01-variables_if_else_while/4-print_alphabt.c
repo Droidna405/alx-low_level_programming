c0b2776c3843a3d8589c99b228317e9b4e374c74
Droidna405 <joemwangala@gmail.com>
2022-09-12 14:33:06 +0000

    exclude letters e and q in alphabet print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet but excludes letters q and e
+ *
+ * Return: zero
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++);
+{
+if (ch != 'e' && ch != 'q');
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
