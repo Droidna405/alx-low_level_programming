91a3850c9a9a42c2e01da56630fc30010fee7551
Droidna405 <joemwangala@gmail.com>
2022-09-12 14:41:16 +0000

    exclude letters e and q in alphabet print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,15 @@
 int main(void)
 {
 char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++);
 {
-for(ch = 'a' ; ch <= 'z' ; ch++);
+if (ch == 'e' && ch == 'q');
 {
-if (ch != 'e' && ch != 'q');
-{
+continue;
+}
 putchar(ch);
 }
-}
 putchar('\n');
-}
 return (0);
 }
