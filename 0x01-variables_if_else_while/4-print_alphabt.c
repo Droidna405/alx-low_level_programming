f2f7faef62ae20d04c13a4c962615cb515fbf5bb
Droidna405 <joemwangala@gmail.com>
2022-09-12 19:43:01 -0700

    printing 0-10

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 char ch;
-
+{
 for (ch = 'a' ; ch <= 'z' ; ch++);
-if (ch != 'e' && ch != 'q');
+if (ch != 'e' && ch != 'q')
 putchar(ch);
+}
 putchar('\n');
 return (0);
 }
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers from base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i);
+}
+printf("\n");
+return (0);
+}
