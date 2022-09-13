4e648c9d40e61281988c3dcc68efe278bb630483
Droidna405 <joemwangala@gmail.com>
2022-09-12 19:31:12 -0700

    Eclude q and e in alphabet print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,10 @@
 int main(void)
 {
 char ch;
+
 for (ch = 'a' ; ch <= 'z' ; ch++);
-{
-if (ch != 'e' && ch != 'q')
-{
+if (ch != 'e' && ch != 'q');
 putchar(ch);
-}
-}
 putchar('\n');
 return (0);
 }
