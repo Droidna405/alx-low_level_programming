7b159939f0c480ef364f3d83b2a856bc6e6e1359
Droidna405 <joemwangala@gmail.com>
2022-09-14 08:16:59 +0000

    Alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "_putchar"
+/**
+ * print_alphabet - print the alphabet in lowercase followed by a new line
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+int c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+  
+  
