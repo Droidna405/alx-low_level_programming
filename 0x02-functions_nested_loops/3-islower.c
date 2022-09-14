d66d5e1db29891c8e77f14af9586c3339278b13b
Droidna405 <joemwangala@gmail.com>
2022-09-14 13:10:40 +0000

    scan and print lowercase letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower(int c) - scans lowercase characters
+ * @c: is the interger that will be used for the argument in the function
+ * Return: 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <+ 'z')
+{
+return (1);
+}
+else
+return (0);
+}
