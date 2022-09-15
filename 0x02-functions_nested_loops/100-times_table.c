3a3638f5bb1445150403e1434f8d0c57038c3811
Droidna405 <joemwangala@gmail.com>
2022-09-15 13:52:50 +0000

    prints 'i' times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+* print_times_table - prints the  times table with
+* parameter
+* @i: parameter
+* Return: returns nothing
+*/
+void print_times_table(int i)
+{
+int digit, mult, result;
+if (i <= 15 && i >= 0)
+{
+for (digit = 0; digit <= i; digit++)
+{
+_putchar('0');
+
+for (mult = 1; mult <= i; mult++)
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 99)
+_putchar(' ');
+
+if (result <= 9)
+_putchar(' ');
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar((result / 10) % 10 + '0');
+}
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
