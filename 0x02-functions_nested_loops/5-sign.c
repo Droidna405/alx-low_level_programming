7f4644b2c2063601db4be7b43d85d7048075eb3f
Droidna405 <joemwangala@gmail.com>
2022-09-15 09:39:31 +0000

    Print sign of integer

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * int print_sign - looks for code in main.h
  * print sign - function to check for a sign of a number
  * @n: is the int that will use for the arguent of the function
  *
