ce33607b4a5c4c298beab3d80809151e746d8c97
Droidna405 <joemwangala@gmail.com>
2022-09-15 13:46:06 +0000

    absolute value of an integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_last_digit(int) - Prints the last digit of a number.
+ * print_last_digit - Prints the last digit of a number.
  * @i: is the integer
  *
  * Return: value of the last digit
  */
-int print_last_digit(int n)
+int print_last_digit(int i)
 {
-int last_digit = n % 10;
+int last_digit = i % 10;
 if (last_digit < 0)
 last_digit *= -1;
 _putchar(last_digit + '0');
