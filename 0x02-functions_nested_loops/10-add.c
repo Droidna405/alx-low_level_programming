2e8b9eb7266222ea780af6548dee95f65157e63a
Droidna405 <joemwangala@gmail.com>
2022-09-15 11:26:14 +0000

    print two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * add - returns sum of two integers
- * @x - first integer
- * @z - second integer
+ * x - first integer
+ * z - second integer
  * Return: 0
  */
 int add(int x, int z)
 {
 return (x + z);
 {
-  _putchar('\n');
+_putchar('\n');
 }
 return (0);
 }
