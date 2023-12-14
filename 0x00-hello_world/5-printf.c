2192b9a20c4f702410a9b740591ea10066137768
Droidna405 <joemwangala@gmail.com>
2023-12-14 02:44:51 -0500

    C program that prints a string using printf followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - begins to initiate the program
+ *
+ * Description: Program that prints "with proper grammar"...
+ *
+ * Return: Success (0)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
