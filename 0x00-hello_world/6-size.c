1c69527fc77c7f16e2f2feccf306ce7caba5401e
Droidna405 <joemwangala@gmail.com>
2023-12-14 04:59:08 -0500

    C program that prints the size of various data types correct

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of char: %lu byte\(s\)\n", sizeof(c));
-printf("Size of int: %lu byte\(s\)\n", sizeof(i));
-printf("Size of long int: %lu byte\(s\)\n", sizeof(li));
-printf("Size of long long int: %lu byte\(s\)\n", sizeof(lli));
-printf("Size of float: %lu byte\(s\)\n", sizeof(f));
+printf("Size of char: %zu byte\(s\)\n", sizeof(c));
+printf("Size of int: %zu byte\(s\)\n", sizeof(i));
+printf("Size of long int: %zu byte\(s\)\n", sizeof(li));
+printf("Size of long long int: %zu byte\(s\)\n", sizeof(lli));
+printf("Size of float: %zu byte\(s\)\n", sizeof(f));
 return (0);
 }
