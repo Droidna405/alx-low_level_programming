cdcc56c0ef0bdbb95fd7f20c73e399da75a91817
Droidna405 <joemwangala@gmail.com>
2023-12-14 02:20:11 -0500

    Printing text on screen using puts on c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This program prints using the function puts
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts(\""Programming is like building a multilingual puzzle"\n);
+return(0);
+}
