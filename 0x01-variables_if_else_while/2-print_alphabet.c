5a93e7b96535eb8ead4542f9ba49c1e68f7f5164
Droidna405 <joemwangala@gmail.com>
2023-12-16 06:16:32 +0300

    Prints alphabet using putchar in small case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point of program
+ *
+ * Description: Prints the alphabet in lowercase
+ * 
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+char n;
+for (n = 'a'; n <= 'z'; ++n)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
