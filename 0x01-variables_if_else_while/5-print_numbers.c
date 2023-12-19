72847d2aff5bfacb7653b98f0679b36b56258363
Droidna405 <joemwangala@gmail.com>
2023-12-19 05:51:01 +0300

    prints numbers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ *
+ * Description: Prints integers 0 - 9 followed by a new line
+ *
+ * Return: Always (0) for success
+ */
+int main(void)
+{
+int n;
+for (n = 0 ; n < 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
+
