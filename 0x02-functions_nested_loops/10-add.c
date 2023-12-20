dabba71b2577318fd6e178b5b4a9a86f3c5cd68c
Droidna405 <joemwangala@gmail.com>
2023-12-19 19:35:20 -0800

    returns the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - returns sum of two integers
+ * @x: integer
+ * @z: integer
+ * Return: 0
+ */
+int add(int x, int z)
+{
+return (x + z);
+{
+_putchar('\n');
+}
+return (0);
+}
