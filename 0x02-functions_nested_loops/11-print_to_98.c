2571df5452d1f6205e8bcd6e859c377fe9aa01ff
Droidna405 <joemwangala@gmail.com>
2023-12-19 19:32:08 -0800

    print a number n to 98 if its <98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from - 98
+ * @n: The number to begin counting at.
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+  if (n >= 98)
+    {
+      while (n > 98)
+	printf("%d, ", n--);
+      printf("%d\n", n);
+    }
+
+  else
+    {
+      while (n < 98)
+	printf("%d, ", n++);
+      printf("%d\n", n);
+    }
+}#include <stdio.h>
+
+ /**
+  * print_to_98 - prints all natural numbers from - 98
+  * @n: The number to begin counting at.
+  * Return: 0
+  */
+void print_to_98(int n)
+{
+  if (n >= 98)
+    {
+      while (n > 98)
+	printf("%d, ", n--);
+      printf("%d\n", n);
+    }
+
+  else
+    {
+      while (n < 98)
+	printf("%d, ", n++);
+      printf("%d\n", n);
+    }
+}#include <stdio.h>
+
+ /**
+  * print_to_98 - prints all natural numbers from - 98
+  * @n: The number to begin counting at.
+  * Return: 0
+  */
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);
+}
+}
