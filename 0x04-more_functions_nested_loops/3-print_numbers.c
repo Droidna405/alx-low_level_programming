f869513c471c95bc0e0110770e8c2838a90ff8dc
Droidna405 <joemwangala@gmail.com>
2023-12-21 16:55:24 +0300

    prints 0-9 using _putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints from 0-9
+ * Returns: (0) when successful
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 48 ; a <= 57; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
