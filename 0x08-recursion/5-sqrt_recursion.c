a0880477b3e6406081242b61c29af4bf8394e3ef
Droidna405 <joemwangala@gmail.com>
2024-01-11 08:33:04 -0500

    Function that looks for the squareroot of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _sqrt_recursion - calculates the natural square root of a number.
  * @n: number to find the square root of
- * Return: natural square root of n, -1 if n does not have a natural square root
+ * Return: square root of n, -1 if n does not have a natural square root
  */
 int _sqrt_recursion(int n)
 {
@@ -16,7 +16,7 @@ return (sqrt_recursive(n, 1));
  * sqrt_recursive - recursive function to find the natural square root
  * @n: number to find the square root of
  * @guess: current guess for the square root
- * Return: natural square root of n, -1 if n does not have a natural square root
+ * Return: sqrt of n, -1 if n does not have a natural square root
  */
 int sqrt_recursive(int n, int guess)
 {
