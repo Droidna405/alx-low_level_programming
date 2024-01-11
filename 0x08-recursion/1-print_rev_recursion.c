8aa51eb10e5f518feb78f516fb4794599ce8ea5f
Droidna405 <joemwangala@gmail.com>
2024-01-11 03:12:47 -0500

    Prints string in reverse with Recursion task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a reverse string using recursion
+ * @s: string to be printed in reverse
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+else
+{
+_putchar('\n');
+}
+}
