684fa9bba4fcb38060c55bfcb2b77f281bd44fff
Droidna405 <joemwangala@gmail.com>
2024-01-22 21:53:10 +0300

    Typedef Struct First commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - represents information about dog
+ * @name: Pointer to a character string representing the dog's name.
+ * @age: Floating-point number representing the dog's age.
+ * @owner: Pointer to a character string representing the dog's owner.
+ * Description: This structure holds information about a dog,
+ * including its name, age, and owner.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
