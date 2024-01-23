9649237628c709a75bdae67c43894d4a68834c5d
Droidna405 <joemwangala@gmail.com>
2024-01-23 08:30:40 -0500

    New dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,14 @@
  * Description: This structure holds information about a dog,
  * including its name, age, and owner.
  */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+}dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 int _putchar(char c);
