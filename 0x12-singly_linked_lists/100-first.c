d7888aed2559e7ef684a5e676581a84bd6d253c9
Droidna405 <joemwangala@gmail.com>
2024-02-02 08:40:07 -0500

    prints a string before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,5 +5,5 @@
 void __attribute__((constructor)) pre_main_message(void)
 {
 printf("You're beat! and yet,you must allow, \n"
-"nI bore my house upon my back!\n");
+"I bore my house upon my back!\n");
 }
