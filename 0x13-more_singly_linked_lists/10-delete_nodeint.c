31b237402773184e4bf2f221b1b717cbd6413256
Droidna405 <joemwangala@gmail.com>
2024-02-06 05:50:31 -0500

    deletes the node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,4 +1,24 @@
 #include "lists.h"
+#include <stdio.h>
+#include "lists.h"
+/**
+ * listint_len - function to count the number of elements in the list
+ * @h: position at node
+ * Return: length
+ */
+size_t listint_len(const listint_t *h)
+{
+
+size_t count = 0;
+
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+
+return (count);
+}
 
 /**
  * delete_nodeint_at_index - deletes the node at a given
