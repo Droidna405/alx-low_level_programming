90153ba6b4b83daaefe0aff202aa740d924dc94d
Droidna405 <joemwangala@gmail.com>
2024-02-06 05:45:43 -0500

    deletes the node at index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at a given
+ * index of a listint_t list
+ * @head: a pointer to a pointer to the head of the list
+ * @index: the index of the node to delete
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *current;
+listint_t *temp;    /* Temporary holder for the node to be deleted */
+unsigned int i;
+/* Check for an empty list or invalid index: */
+if (*head == NULL || index >= listint_len(*head))
+{
+return (-1); /* failure */
+}
+if (index == 0)
+{
+temp = *head;
+*head = (*head)->next;
+free(temp);
+return (1);
+}
+current = *head;
+for (i = 1; i < index && current != NULL; i++)
+{
+current = current->next;
+}
+
+if (current == NULL || current->next == NULL)
+{
+return (-1);
+}
+
+/* Delete the node: */
+temp = current->next;
+current->next = temp->next;
+free(temp);  /* Frees the removed carriage */
+
+return (1);  /* Indicates success */
+}
