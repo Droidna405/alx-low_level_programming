8e154e0dbb8f403bb87a68d785dd503881bad2a1
Droidna405 <joemwangala@gmail.com>
2024-02-07 02:45:10 -0500

    0x0B Malloc tasks correction

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,33 +13,26 @@ int **alloc_grid(int width, int height)
 int **grid;
 int i, j;
 
-/* Handle invalid dimensions */
-if (width <= 0 || height <= 0) {
+if (width <= 0 || height <= 0)
 return (NULL);
-}
 
-/* Allocate memory for the array of pointers to rows */
 grid = (int **)malloc(height * sizeof(int *));
-if (grid == NULL) {
-return (NULL); /* Allocation failure */
-}
+if (grid == NULL)
+return (NULL);
 
-/* Allocate memory for each row and initialize elements to 0 */
-for (i = 0; i < height; i++) {
+for (i = 0; i < height; i++)
+{
 grid[i] = (int *)malloc(width * sizeof(int));
-if (grid[i] == NULL) {
-
-/* Free previously allocated rows on failure */
-for (j = 0; j < i; j++) {
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
 free(grid[j]);
-}
 free(grid);
 return (NULL);
 }
 
-for (j = 0; j < width; j++) {
+for (j = 0; j < width; j++)
 grid[i][j] = 0;
-}
 }
 
 return (grid);
