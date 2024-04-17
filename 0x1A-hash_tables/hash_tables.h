92b198d18bdfb2b8a2c9f38a206987cb0c156e76
Droidna405 <joemwangala@gmail.com>
2024-04-17 05:31:42 -0400

    Creates a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -12,9 +12,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -27,8 +27,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 
 /* Function prototypes */
@@ -40,12 +40,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-/* Rewrite of functions for advanced task */
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
 
+/* Rewrite of functions for advanced task */
+/*
+ *shash_table_t *shash_table_create(unsigned long int size);
+ *int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+ *char *shash_table_get(const shash_table_t *ht, const char *key);
+ *void shash_table_print(const shash_table_t *ht);
+ *void shash_table_print_rev(const shash_table_t *ht);
+ *void shash_table_delete(shash_table_t *ht);
+ */
 #endif /* HASH_TABLES_H */
