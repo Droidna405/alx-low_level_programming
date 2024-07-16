a1a2d767b1e33e97a56f62a6afee445a1731c9f7
Droidna405 <joemwangala@gmail.com>
2024-07-16 16:34:58 +0300

    Binary Search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,13 +13,20 @@ int binary_search(int *array, size_t size, int value)
 size_t low = 0;
 size_t high = size - 1;
 size_t mid;
+size_t i;
 
 while (low <= high)
 {
 mid = low + (high - low) / 2;
 
-printf("Searching in array[%lu...%lu]\n", low, high);
-
+printf("Searching in array: ");
+for (i = low; i <= high; i++)
+{
+printf("%d", array[i]);
+if (i < high)
+printf(", ");
+}
+printf("\n");
 if (array[mid] == value)
 return (mid);
 else if (array[mid] < value)
